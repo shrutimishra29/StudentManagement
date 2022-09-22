4b17d8511cbbafebf296c8378f1aaddca1697dc9
Shruti Mishra <shruti_mishra@persistent.com>
2022-09-22 16:41:46 +0530

    first commit

---
+++ Management/src/main/java/com/college/Management/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.college.Management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @RequestMapping("/help")
+    public String help(){
+        return "faq";
+    }
+
+//    @RequestMapping("/error")
+//    public String error(){
+//     return "error";
+//    }
+}
