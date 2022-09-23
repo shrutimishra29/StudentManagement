455bd52230ea5def285ef560804b84d2ae14a5b3
Shruti Mishra <shruti_mishra@persistent.com>
2022-09-23 12:10:32 +0530

    somethings idk

--- Management/src/main/java/com/college/Management/controller/HomeController.java
+++ Management/src/main/java/com/college/Management/controller/HomeController.java
@@ -3,24 +3,28 @@ package com.college.Management.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
-
+    //Request mapping for home page
     @RequestMapping("/")
     public String home() {
         return "home";
     }
 
+    //Request mapping for home page
     @RequestMapping("/about")
     public String about() {
         return "about";
     }
 
+    //Request Mapping for help page
     @RequestMapping("/help")
     public String help() {
         return "faq";
     }
 
+    //Request Mapping for contact page
     @RequestMapping("/contact")
     public String contact() {
         return "contact";
--- Management/src/main/java/com/college/Management/controller/UserController.java
+++ Management/src/main/java/com/college/Management/controller/UserController.java
@@ -1,17 +1,55 @@
 package com.college.Management.controller;
 
+import com.college.Management.entities.Users;
+import com.college.Management.services.UserDbService;
+import org.omg.CORBA.SystemException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    UserDbService dbService;
+
+    //Request mapping for register page
     @RequestMapping("register")
     public String register(){
         return "register";
     }
 
+    //Reuqest mapping for login page
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
+
+    @RequestMapping(path="/loggedin")
+    public String loggedIn(HttpServletRequest request) throws IllegalStateException, SystemException {
+        //	String email = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username + " " + password);
+        if(dbService.validate(username, password)){
+            return "success";
+        }
+        else {
+            return "invalidlogin";
+        }
+    }
+
+    @RequestMapping(path = "/processform")
+    public String newUser(HttpServletRequest request) {
+        String email = request.getParameter("email");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        Users user = new Users(username,email,password);
+        System.out.println(user.getUsername() + user.getEmail() + user.getPassword());
+        dbService.create(user);
+        return "success";
+    }
+
 }
